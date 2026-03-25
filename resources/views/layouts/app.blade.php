96047691584b18cedb0cbd03d9c36b18b764ea53
James Philip Gomera <jpgomera19@gmail.com>
2026-03-25 11:17:23 +0800

    Removed the chatbot

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,23 +58,23 @@
         window.addEventListener('touchstart', initLandbot, {
             once: true
         });
-        var myLandbot;
-
-        function initLandbot() {
-            if (!myLandbot) {
-                var s = document.createElement('script');
-                s.type = "module"
-                s.async = true;
-                s.addEventListener('load', function () {
-                    var myLandbot = new Landbot.Livechat({
-                        configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3236836-KDF61WB61CG5PRC2/index.json',
-                    });
-                });
-                s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
-                var x = document.getElementsByTagName('script')[0];
-                x.parentNode.insertBefore(s, x);
-            }
-        }
+        // var myLandbot;
+        //
+        // function initLandbot() {
+        //     if (!myLandbot) {
+        //         var s = document.createElement('script');
+        //         s.type = "module"
+        //         s.async = true;
+        //         s.addEventListener('load', function () {
+        //             var myLandbot = new Landbot.Livechat({
+        //                 configUrl: 'https://storage.googleapis.com/landbot.online/v3/H-3236836-KDF61WB61CG5PRC2/index.json',
+        //             });
+        //         });
+        //         s.src = 'https://cdn.landbot.io/landbot-3/landbot-3.0.0.mjs';
+        //         var x = document.getElementsByTagName('script')[0];
+        //         x.parentNode.insertBefore(s, x);
+        //     }
+        // }
     </script>
 </body>
 
